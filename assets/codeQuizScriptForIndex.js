64f1b0622d34ad37dff298e3514c9d3ba47e02e5
Kevponce <Kevponce194@gmail.com>
2020-10-06 14:00:15 -0400

    add styling to questions/choices
    still need to link score data
    
    still need to link to high score page'
    git push
    git status
    git add .

--- assets/codeQuizScriptForIndex.js
+++ assets/codeQuizScriptForIndex.js
@@ -10,6 +10,7 @@ var questions = [{
   },
 ];
 
+var questionNumEl = document.querySelector("#questionNum");
 var questionEl = document.querySelector("#question");
 var optionListEl = document.querySelector("#option-list");
 var questionResultEl = document.querySelector("#question-result");
@@ -27,7 +28,7 @@ function endQuiz() { //step 4- ends the quiz and calls show high score
   setTimeout(showHighScore, 2); // wait 2 seconds then call function to render high score page
 }
 //_____________________________________________________________________________
-function showHighScore() {  // step 5 clear page and show high score
+function showHighScore() { // step 5 clear page and show high score
   var name = prompt("Please enter your name"); //ask user to enter their name
   //validation?
 
@@ -48,7 +49,7 @@ function showHighScore() {  // step 5 clear page and show high score
 
   localStorage.setItem("scores", JSON.stringify(high_scores)); //adds item to local storage 
 
-  high_scores.sort(function (a, b) {  //  sort through array... 
+  high_scores.sort(function (a, b) { //  sort through array... 
     return b.score - a.score; // ... and sort in descending order
   });
 
@@ -58,7 +59,7 @@ function showHighScore() {  // step 5 clear page and show high score
     var contentLI = document.createElement("li"); // ... and create an li's for the above ul
     contentLI.textContent =
       "Name: " + high_scores[i].name + " Score: " + high_scores[i].score; //set the textContent for the li's
-    contentUL.appendChild(contentLI);  //  append the li's to the ul
+    contentUL.appendChild(contentLI); //  append the li's to the ul
   }
 
   document.body.appendChild(contentUL); //  append the ul to the body
@@ -66,37 +67,39 @@ function showHighScore() {  // step 5 clear page and show high score
 //_____________________________________________________________________________
 function updateTime() { // step 3a - called by step 2-renderQuestion to decrement time or endQuiz
   time--; // decrements the time
-  timerEl.textContent = time; // updates DOM time counter
+  timerEl.textContent = "Time Left: " + time; // updates DOM time counter
   if (time <= 0) { // if time expires, or time subtraction forces value <=0 call endQuiz
     endQuiz(); // calls endQuiz()
   }
 }
 //_____________________________________________________________________________
-function renderQuestion() {   // step 2 - renders question and starts timer
+function renderQuestion() { // step 2 - renders question and starts timer
 
-  if (time === 0) {  // checks to see if still time, 
-    updateTime();  // if no time calls updateTime
+  if (time === 0) { // checks to see if still time, 
+    updateTime(); // if no time calls updateTime   ---activate after styling
     return; //  why is this return needed ?
   }
 
   // if time is left calls updateTime at 1 second intervals
-  intervalId = setInterval(updateTime, 1000); //add timer that will call updateTime every second
+  // intervalId = setInterval(updateTime, 1000); //add timer that will call updateTime every second
+  questionNumEl.textContent = "Question " + (questionIndex + 1) + ":"; //trying to get Question number
   questionEl.textContent = questions[questionIndex].question; //renders current question to DOM
 
   optionListEl.innerHTML = ""; // clears the optionListEl
   questionResultEl.innerHTML = ""; // clears the questionResultEl
 
-  var choices = questions[questionIndex].choices;  // easier way to write the right side of the expression
+  var choices = questions[questionIndex].choices; // easier way to write the right side of the expression
   // var choicesLength = choices.length;
 
   for (var i = 0; i < choices.length; i++) {
     var questionListItem = document.createElement("li");
     questionListItem.textContent = choices[i];
+    questionListItem.setAttribute("class", "btn");
     optionListEl.append(questionListItem);
   }
 }
 //_____________________________________________________________________________
-function nextQuestion() {  //  step 4 - increments index, checks to see if more questions; if not sets time to 0, then goes back to step 1 - renderQuestion
+function nextQuestion() { //  step 4 - increments index, checks to see if more questions; if not sets time to 0, then goes back to step 1 - renderQuestion
   questionIndex++; //increments the Question Index by 1
   if (questionIndex === questions.length) { // if no more questions...
     time = 0; // ... sets time to zero, thereby ends game...
@@ -109,9 +112,11 @@ function checkAnswer(event) { // step 3c is called  by Event listener - checks i
   if (event.target.matches("li")) { //checks to make sure you click an li
     var answer = event.target.textContent; //grabs the clicked on choice
     if (answer === questions[questionIndex].answer) { // checks real vs. clicked answer
+      questionResultEl.setAttribute("class", "correct"); // colors the text Blue
       questionResultEl.textContent = "Correct"; // Notify user response is correct by updating DOM
       correctCount++; //Update the correctCount if necessary
     } else {
+      questionResultEl.setAttribute("class", "incorrect"); // colors the text Blue
       questionResultEl.textContent = "Incorrect"; // Notify user response is Incorrect by updating DOM
       time = time - 2; // subtract 2 seconds from time.
       timerEl.textContent = time; // updates DOM timer counter
